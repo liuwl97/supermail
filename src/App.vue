f17132364735e787625ab17f743631a99101ce43
liuwl <2516657784@qq.com>
2021-05-02 22:47:37 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  
+    <router-view></router-view>
+    <tab-main /> 
 </template>
 
 <script>
+import TabMain from 'components/content/TabMain';
   export default{
     name:"app",
+    components:{
+      TabMain
+    }
   }
 </script>
 <style lang="scss">
-
+  @import "assets/css/base.css"; 
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 
+//下面为懒加载形式
+const Home = () => import('../views/home/Home.vue')
+const Category = () => import('../views/category/Category.vue')
+const Order = () => import('../views/order/Order.vue')
+const Profile = () => import('../views/profile/Profile.vue')
 const routes: Array<RouteRecordRaw> = [
-  
+
+  //tabbar
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component: Home
+  },
+  {
+    path:'/category',
+    component: Category
+  },
+  {
+    path:'/order',
+    component: Order
+  },
+  {
+    path:'/profile',
+    component: Profile
+  }
 ]
 
 const router = createRouter({
@@ -11,3 +37,4 @@ const router = createRouter({
 })
 
 export default router
+
